6ac2cc41ed9449e679a3d37ecdd909e5197179db
ItaMus90 <itamarmuslvi@gmail.com>
2019-09-06 11:31:43 +0300

    create header and footer and include them in views file

--- app/views/pages/index.php
+++ app/views/pages/index.php
@@ -1,14 +1,6 @@
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Home Page</title>
-</head>
-<body>
-    <h1>Home Page</h1>
-    <h4><?php echo $data["title"]; ?></h4>
-</body>
-</html>
+<?php require APP_ROOT . "/views/inc/header.php"; ?>
+
+
+<h1>Hello</h1>
+
+<?php require APP_ROOT . "/views/inc/footer.php"; ?>
