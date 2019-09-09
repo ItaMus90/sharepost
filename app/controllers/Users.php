86ce5ff6a3889fd07a2e5b38025d390401f74586
ItaMus90 <itamarmuslvi@gmail.com>
2019-09-09 16:24:08 +0300

    create Users controller add register method build view for users register page

---
+++ app/controllers/Users.php
@@ -0,0 +1,44 @@
+<?php
+
+
+class Users extends  BaseController {
+
+
+
+
+    public function __construct(){
+
+
+
+    }
+
+
+    public function register(){
+
+        //Check for POST
+        if ($_SERVER["REQUEST_METHOD"] == "POST"){
+
+            //Process Form
+
+        }else {
+
+            //Init data
+            $data = array(
+                "name"                 => "",
+                "email"                => "",
+                "password"             => "",
+                "confirm_password"     => "",
+                "name_err"             => "",
+                "email_err"            => "",
+                "password_err"         => "",
+                "confirm_password_err" => "",
+            );
+
+
+            //Load Views
+            $this->view('users/register', $data);
+
+        }
+
+    }
+}
